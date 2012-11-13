b7e499f1b809588fbecab483dfa2935514cb4000
Luvic van Gool <vic@thecloudblocks.com>
2012-11-13 16:32:22 +0000

    increased job check to 60 seconds

--- lib/version.rb
+++ lib/version.rb
@@ -13,7 +13,7 @@ module Agent
 		#  Defines the minor version
 		# PATCH:
 		#  Defines the patch version
-		MAJOR, MINOR, PATCH  = 0, 0, 15
+		MAJOR, MINOR, PATCH  = 0, 0, 16
 
 		#ie. PRERELEASE_MODIFIER = 'beta1'
 		PRERELEASE_MODIFIER = nil
