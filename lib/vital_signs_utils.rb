d51d457a7a4f9731b202b2c11344f7ada9a3e393
Luvic van Gool <vic@thecloudblocks.com>
2012-12-06 15:57:12 +0000

    adding ip detection

--- lib/vital_signs_utils.rb
+++ lib/vital_signs_utils.rb
@@ -5,7 +5,6 @@ require 'open-uri'
 class VitalSignsUtils
 
 	def self.get_free_space_info
-
 		space_info = { }
 		Sys::Filesystem.mounts do |mount|
 			stat = Sys::Filesystem.stat(mount.mount_point)
@@ -19,19 +18,16 @@ class VitalSignsUtils
 			space_info[mount.mount_point] = { mb_free: mb_free, mb_total: mb_total }
 			mount.mount_point
 		end
-
 		return space_info
 	end
 
 	def self.get_network_info
-
 		address_infos = Socket.ip_address_list
 		private_ip_addresses = address_infos.select { |intf| intf.ipv4_private? }.map { |address| address.ip_address }
 		public_ip_addresses = address_infos.select { |intf| intf.ipv4? and !intf.ipv4_loopback? and !intf.ipv4_multicast? and !intf.ipv4_private? }.map { |address| address.ip_address }
 
-		ipaddr = UDPSocket.open {|s| s.connect('65.59.196.211'); s.addr.last }
-
+		return { private: private_ip_addresses, public: public_ip_addresses }
 	end
-
 end
+
 
