4505680bbccd56e5097710e29d9a94a4df7db582
Luvic van Gool <vic@thecloudblocks.com>
2013-02-28 16:06:26 +0000

    memory different metric

--- lib/version.rb
+++ lib/version.rb
@@ -13,7 +13,7 @@ module Agent
 		#  Defines the minor version
 		# PATCH:
 		#  Defines the patch version
-		MAJOR, MINOR, PATCH  = 0, 0, 25
+		MAJOR, MINOR, PATCH  = 0, 0, 26
 
 		#ie. PRERELEASE_MODIFIER = 'beta1'
 		PRERELEASE_MODIFIER = nil
--- lib/vital_signs_utils.rb
+++ lib/vital_signs_utils.rb
@@ -20,6 +20,9 @@ class VitalSignsUtils
 			mount.mount_point
 		end
 		return space_info
+	rescue => exc
+		@log.error "Failure during disk usage gathering due to #{exc}"
+		return { invalid: exc.message }
 	end
 
 	def self.get_cpu_usage_info
@@ -61,58 +64,45 @@ class VitalSignsUtils
 
 		#get average utilization value
 		return { percent_used: percent_used, percent_free: idle_value }
+	rescue => exc
+		@log.error "Failure during CPU usage gathering due to #{exc}"
+		return { invalid: exc.message }
 	end
 
 	def self.get_memory_usage_info
-
-		#parse sar result
-		sar_result = `sar -r 1 5`
-
-#		sar_result = <<-SAMPLE
-#Linux 3.2.0-31-virtual (ip-10-30-159-183) 	12/07/2012 	_x86_64_	(2 CPU)
-#
-#12:23:38 PM kbmemfree kbmemused  %memused kbbuffers  kbcached  kbcommit   %commit  kbactive   kbinact
-#12:23:39 PM    436404   1296860     74.82     68204    693436    959636     36.20    800452    385916
-#12:23:40 PM    436156   1297108     74.84     68204    693436    959704     36.20    800436    385916
-#12:23:41 PM    436164   1297100     74.84     68204    693436    959704     36.20    800376    385916
-#12:23:42 PM    436032   1297232     74.84     68204    693436    959704     36.20    800520    385916
-#12:23:43 PM    436048   1297216     74.84     68204    693436    959704     36.20    800496    385916
-#12:23:44 PM    436032   1297232     74.84     68204    693436    959704     36.20    800440    385916
-#12:23:45 PM    436040   1297224     74.84     68204    693436    959704     36.20    800520    385916
-#12:23:46 PM    436040   1297224     74.84     68204    693436    959704     36.20    800436    385916
-#12:23:47 PM    436040   1297224     74.84     68204    693436    959704     36.20    800436    385916
-#12:23:48 PM    436048   1297216     74.84     68204    693436    959704     36.20    800436    385916
-#Average:       436100   1297164     74.84     68204    693436    959697     36.20    800455    385916
+		free_m_result = `free -m`
+#		free_m_result = <<-SAMPLE
+#             total       used       free     shared    buffers     cached
+#Mem:           590        480        109          0         37        227
+#-/+ buffers/cache:        216        373
+#Swap:            0          0          0
 #SAMPLE
 
-#split output into lines
-		lines = sar_result.split(/\r?\n/)
+		free_m_result.each_line do |line|
+			if line =~ /^Mem:/
+				parts = line.scan(/.*?(\d+)/)
+				parts.flatten!
 
-		#get rid of time (first 13 chars)
-		lines = lines.map { |line| line[12..-1].strip unless line[12..-1].nil? }
+				mb_total = parts[0].to_f
+				# mb_used is not a true representation due to OS gobbling up mem
+				# mb_used = parts[1].to_i
+				mb_free = parts[2].to_f
+				mb_shared = parts[3].to_f
+				mb_buffers = parts[4].to_f
+				mb_cached = parts[5].to_f
 
-		#get the header line and split into columns
-		header_line = lines.detect { |line| line =~ /kbmemfree/ }
-		columns = header_line.split(/\s+/)
-
-		#detect positions of memfree and memused columns
-		mem_free_index = columns.index('kbmemfree')
-		mem_used_index = columns.index('kbmemused')
-
-		#get average line
-		average_line = lines[-1]
-		columns = average_line.split(/\s+/)
-
-		#get values in mb
-		mb_free_string = columns[mem_free_index]
-		mb_free = mb_free_string.to_f / 1024
-		mb_used_string = columns[mem_used_index]
-		mb_used = mb_used_string.to_f / 1024
-		mb_total = mb_free + mb_used
-		percent_used = mb_used / mb_total * 100
-
-		#get average utilization value
-		return { mb_free: mb_free, mb_used: mb_used, mb_total: mb_total, percent_used: percent_used }
+				#The total free memory available to proceses is calculated by adding up Mem:cached + Mem:buffers + Mem:free (99 + 63 + 296)
+				#This then needs to be divided by Mem:total to get the total available free memory (1692)
+				mb_available = mb_cached + mb_buffers + mb_free
+				mb_used = mb_total - mb_available
+				mb_free = mb_total - mb_used
+				percent_used = mb_used / mb_total * 100
+				return { mb_free: mb_free, mb_used: mb_used, mb_total: mb_total, percent_used: percent_used }
+			end
+		end
+	rescue => exc
+		@log.error "Failure during memory usage gathering due to #{exc}"
+		return { invalid: exc.message }
 	end
 
 	def self.get_facter_info
